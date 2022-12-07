aa9eb837c6f2e9d2260064ea19aa57cc54441be1
Eduardo Casseano <eduardo@ehc.io>
2022-12-06 21:28:31 -0300

    iptables script + PBR

--- mig-nva.tf
+++ mig-nva.tf
@@ -1,3 +1,7 @@
+
+#########################################################################
+# Instance Managed Group - MIG (Network Virtual Appliance/NVA)
+#########################################################################
 data "google_compute_image" "default" {
   family  = "ubuntu-2004-lts"
   project = "ubuntu-os-cloud"
@@ -12,8 +16,7 @@ resource "google_compute_disk" "default" {
         depends_on = [
         module.host_project.name
         ]
-    }
-
+}
 resource "google_compute_instance_template" "nva-template" {
     project      = module.host_project.name
     name        = "nva-template"
@@ -28,6 +31,8 @@ resource "google_compute_instance_template" "nva-template" {
         #!/bin/bash
         apt update -y
         apt install -y net-tools traceroute
+        # nginx for nva health-checks
+        apt install -y nginx
         # static routes
         route add -net 192.168.30.0 netmask 255.255.255.0 gw 192.168.20.1
         route add -net 192.168.40.0 netmask 255.255.255.0 gw 192.168.20.1
@@ -41,15 +46,20 @@ resource "google_compute_instance_template" "nva-template" {
         apt install -y iptables-persistent
         sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
         sysctl -p
-        # SNAT ens4 config
+        # SNAT ens4 - egress
         iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
+        # SNAT ens5 - ingress
+        iptables -t nat -A POSTROUTING -o ens5 -j MASQUERADE
+        # SNAT ens6 - ingress
+        iptables -t nat -A POSTROUTING -o ens6 -j MASQUERADE
         # DNAT config
-        # md_vm="http://metadata.google.internal/computeMetadata/v1/instance/"
-        # md_net="$md_vm/network-interfaces"
-        # dnat_ip=$(curl -s $md_net/0/ip -H "Metadata-Flavor:Google")
-        # iptables -t nat -A PREROUTING -d $dnat_ip -p tcp --dport 80 -j DNAT --to 192.168.30.5
-        # iptables -t nat -A PREROUTING -d $dnat_ip -p tcp --dport 8081 -j DNAT --to 192.168.30.5
-        # iptables -t nat -A PREROUTING -d $dnat_ip -p tcp --dport 8082 -j DNAT --to 192.168.60.5
+        md_vm="http://metadata.google.internal/computeMetadata/v1/instance/"
+        md_net="$md_vm/network-interfaces"
+        dnat_ip=$(curl -s $md_net/0/ip -H "Metadata-Flavor:Google")
+        iptables -t nat -A PREROUTING -d $dnat_ip -p tcp --dport 8081 -j DNAT --to 192.168.30.5
+        iptables -t nat -A PREROUTING -d $dnat_ip -p tcp --dport 8082 -j DNAT --to 192.168.60.5
+        # setup pbr for ILBaNH health checks
+        bash <(curl -s https://gist.githubusercontent.com/ehc-io/2205a52475a915f0321e11afe0af6ff9/raw/93f7bd11d5f6b0c39fa965c7241ffae7f00a3a6b/bashpbr.sh)
         EOT
     }
 
@@ -94,28 +104,28 @@ resource "google_compute_instance_group_manager" "mig-nva" {
     project            = module.host_project.name
     name               = "mig-nva"
     zone               =  var.zone_1
-    target_size        = 1
+    target_size        = 2
     base_instance_name = "nva"
     named_port {
         name = "http"
         port = "80"
     }
-    # named_port {
-    #   name = "web1"
-    #   port = "8081" 
-    # }
-    # named_port {
-    #   name = "web2"
-    #   port = "8082"
+    named_port {
+      name = "web1"
+      port = "8081" 
+    }
+    named_port {
+      name = "web2"
+      port = "8082"
 
-    # }
+    }
     version {
         instance_template = google_compute_instance_template.nva-template.id
         name = "primary"
     }
 }
 #########################################################################
-# External LB - subnet-a
+# External LB-1 - INGRESS - subnet-c
 #########################################################################
 # external ip
 resource "google_compute_global_address" "external-ip-1" {
@@ -129,7 +139,7 @@ resource "google_compute_health_check" "nva-mig-hc-ingress-1" {
     project             = module.host_project.name
     name                = "nva-mig-hc-ingress-1"
     http_health_check {
-        port = 80
+        port = 8081
         port_specification = "USE_FIXED_PORT"
     }
     check_interval_sec  = 3
@@ -140,10 +150,9 @@ resource "google_compute_health_check" "nva-mig-hc-ingress-1" {
 resource "google_compute_backend_service" "bs-vpc-externo-ingress-1" {
     project               = module.host_project.name
     name                  = "bs-vpc-externo-ingress-1"
-    provider              = google-beta
     load_balancing_scheme = "EXTERNAL_MANAGED"
     protocol              = "HTTP"
-    port_name             = "http"
+    port_name             = "web1"
     health_checks         = [ google_compute_health_check.nva-mig-hc-ingress-1.id ]
     backend {
         max_rate = 100
@@ -152,16 +161,16 @@ resource "google_compute_backend_service" "bs-vpc-externo-ingress-1" {
     }
 }
 # url map elb-1
-resource "google_compute_url_map" "default-1" {
+resource "google_compute_url_map" "lb-external-1" {
     project         = module.host_project.name
-    name            = "url-map-default-1"
+    name            = "lb-external-1"
     default_service = google_compute_backend_service.bs-vpc-externo-ingress-1.id
 }
 # http proxy elb-1
 resource "google_compute_target_http_proxy" "default-1" {
     project         = module.host_project.name
     name    = "http-proxy-default-1"
-    url_map = google_compute_url_map.default-1.id
+    url_map = google_compute_url_map.lb-external-1.id
 }
 # forwarding rule elb-1
 resource "google_compute_global_forwarding_rule" "fr-vpc-externo-ingress-1" {
@@ -173,21 +182,70 @@ resource "google_compute_global_forwarding_rule" "fr-vpc-externo-ingress-1" {
     target                = google_compute_target_http_proxy.default-1.id
     ip_address            = google_compute_global_address.external-ip-1.id
 }
-#
-########################################################################
-# health check ilb egress
-resource "google_compute_region_health_check" "nva-mig-hc-egress" {
+#########################################################################
+# External LB-2 - INGRESS - subnet-f
+#########################################################################
+# external ip
+resource "google_compute_global_address" "external-ip-2" {
+    name            = "external-ip-2"
     project         = module.host_project.name
-    name            = "nva-mig-hc-egress"
-    region          = var.region_1
-    tcp_health_check {
-        port = "22"
+    address_type    = "EXTERNAL"
+    ip_version      = "IPV4"
+}
+# health check elb ingress-2
+resource "google_compute_health_check" "nva-mig-hc-ingress-2" {
+    project             = module.host_project.name
+    name                = "nva-mig-hc-ingress-2"
+    http_health_check {
+        port = 8082
+        port_specification = "USE_FIXED_PORT"
+    }
+    check_interval_sec  = 3
+    healthy_threshold   = 2
+    timeout_sec         = 1
+}
+# external backend service-2
+resource "google_compute_backend_service" "bs-vpc-externo-ingress-2" {
+    project               = module.host_project.name
+    name                  = "bs-vpc-externo-ingress-2"
+    load_balancing_scheme = "EXTERNAL_MANAGED"
+    protocol              = "HTTP"
+    port_name             = "web2"
+    health_checks         = [ google_compute_health_check.nva-mig-hc-ingress-1.id ]
+    backend {
+        max_rate = 100
+        balancing_mode = "RATE"
+        group           = google_compute_instance_group_manager.mig-nva.instance_group
     }
 }
-# ILBaNH subnet-B
-resource "google_compute_region_backend_service" "bs-egress-shared-vpc" {
+# url map elb-2
+resource "google_compute_url_map" "lb-external-2" {
+    project         = module.host_project.name
+    name            = "lb-external-2"
+    default_service = google_compute_backend_service.bs-vpc-externo-ingress-2.id
+}
+# http proxy elb-2
+resource "google_compute_target_http_proxy" "default-2" {
+    project         = module.host_project.name
+    name    = "http-proxy-default-2"
+    url_map = google_compute_url_map.lb-external-2.id
+}
+# forwarding rule elb-2
+resource "google_compute_global_forwarding_rule" "fr-vpc-externo-ingress-2" {
+    project         = module.host_project.name
+    name                  = "fr-vpc-externo-ingress-2"
+    ip_protocol           = "TCP"
+    load_balancing_scheme = "EXTERNAL_MANAGED"
+    port_range            = "80-80"
+    target                = google_compute_target_http_proxy.default-2.id
+    ip_address            = google_compute_global_address.external-ip-2.id
+}
+########################################################################
+# ILBaNH EGRESS - subnet-B
+########################################################################
+resource "google_compute_region_backend_service" "ilb-egress-bs-shared-vpc" {
     project               = module.host_project.name
-    name                  = "bs-egress-shared-vpc"
+    name                  = "ilb-egress-bs-shared-vpc"
     region                = var.region_1
     protocol              = "TCP"
     load_balancing_scheme = "INTERNAL"
@@ -199,10 +257,10 @@ resource "google_compute_region_backend_service" "bs-egress-shared-vpc" {
     }
 }
 # ILBaNH subnet-B forwarding rule
-resource "google_compute_forwarding_rule" "fr-egress-shared-vpc" {
+resource "google_compute_forwarding_rule" "ilb-egress-fr-shared-vpc" {
     project               = module.host_project.name
-    name                  = "fr-egress-shared-vpc"
-    backend_service       = google_compute_region_backend_service.bs-egress-shared-vpc.id
+    name                  = "ilb-egress-fr-shared-vpc"
+    backend_service       = google_compute_region_backend_service.ilb-egress-bs-shared-vpc.id
     region                = var.region_1
     ip_protocol           = "TCP"
     load_balancing_scheme = "INTERNAL"
@@ -210,41 +268,48 @@ resource "google_compute_forwarding_rule" "fr-egress-shared-vpc" {
     network               = google_compute_network.vpc_shared.id
     subnetwork            = google_compute_subnetwork.subnet-b.id
 }
-# # #########################################################################
-# # ILBaNH subnet-E
-# resource "google_compute_region_backend_service" "bs-egress-multi-tenant-vpc" {
+#########################################################################
+# ILBaNH subnet-E
+#######################################################################
+resource "google_compute_region_backend_service" "ilb-egress-bs-multi-tenant-vpc" {
+    project               = module.host_project.name
+    name                  = "ilb-egress-bs-multi-tenant-vpc"
+    region                = var.region_1
+    protocol              = "TCP"
+    load_balancing_scheme = "INTERNAL"
+    network               = google_compute_network.vpc_multi_tenant.id
+    health_checks         = [ google_compute_region_health_check.nva-mig-hc-egress.id ]
+    backend {
+        balancing_mode = "CONNECTION"
+        group           = google_compute_instance_group_manager.mig-nva.instance_group
+    }
+}
+# ILBaNH subnet-E forwarding rule
+resource "google_compute_forwarding_rule" "ilb-egress-fr-multi-tenant-vpc" {
+    project         = module.host_project.name
+    name                  = "ilb-egress-fr-multi-tenant-vpc"
+    backend_service       = google_compute_region_backend_service.ilb-egress-bs-multi-tenant-vpc.id
+    region                = var.region_1
+    ip_protocol           = "TCP"
+    load_balancing_scheme = "INTERNAL"
+    all_ports             = true
+    network               = google_compute_network.vpc_multi_tenant.id
+    subnetwork            = google_compute_subnetwork.subnet-e.id
+}
+resource "google_compute_region_health_check" "nva-mig-hc-egress" {
+    project         = module.host_project.name
+    name            = "nva-mig-hc-egress"
+    region          = var.region_1
+    http_health_check {
+        port = "80"
+    }
+}
+#########################################################################
+# ILBaNH subnet-H
+########################################################################
+# resource "google_compute_region_backend_service" "ilb-egress-bs-single-tenant-vpc" {
 #     project               = module.host_project.name
-#     name                  = "bs-egress-multi-tenant-vpc"
-#     provider              = google-beta
-#     region                = var.region_1
-#     protocol              = "TCP"
-#     load_balancing_scheme = "INTERNAL"
-#     network               = google_compute_network.vpc_multi_tenant.id
-#     health_checks         = [ google_compute_region_health_check.nva-mig-hc-egress.id ]
-#     backend {
-#         balancing_mode = "CONNECTION"
-#         group           = google_compute_instance_group_manager.mig-nva.instance_group
-#     }
-# }
-# # ILBaNH subnet-E forwarding rule
-# resource "google_compute_forwarding_rule" "fr-egress-multi-tenant-vpc" {
-#     project         = module.host_project.name
-#     name                  = "fr-egress-multi-tenant-vpc"
-#     backend_service       = google_compute_region_backend_service.bs-egress-multi-tenant-vpc.id
-#     provider              = google-beta
-#     region                = var.region_1
-#     ip_protocol           = "TCP"
-#     load_balancing_scheme = "INTERNAL"
-#     all_ports             = true
-#     network               = google_compute_network.vpc_multi_tenant.id
-#     subnetwork            = google_compute_subnetwork.subnet-f.id
-# }
-# # #########################################################################
-# # ILBaNH subnet-H
-# resource "google_compute_region_backend_service" "bs-egress-single-tenant-vpc" {
-#     project               = module.host_project.name
-#     name                  = "bs-egress-single-tenant-vpc"
-#     provider              = google-beta
+#     name                  = "ilb-egress-bs-single-tenant-vpc"
 #     region                = var.region_1
 #     protocol              = "TCP"
 #     load_balancing_scheme = "INTERNAL"
@@ -256,10 +321,10 @@ resource "google_compute_forwarding_rule" "fr-egress-shared-vpc" {
 #     }
 # }
 # # ILBaNH subnet-H forwarding rule
-# resource "google_compute_forwarding_rule" "fr-egress-single-tenant-vpc" {
+# resource "google_compute_forwarding_rule" "ilb-egress-fr-single-tenant-vpc" {
 #     project         = module.host_project.name
-#     name                  = "fr-egress-single-tenant-vpc"
-#     backend_service       = google_compute_region_backend_service.bs-egress-single-tenant-vpc.id
+#     name                  = "ilb-egress-fr-single-tenant-vpc"
+#     backend_service       = google_compute_region_backend_service.ilb-egress-bs-single-tenant-vpc.id
 #     provider              = google-beta
 #     region                = var.region_1
 #     ip_protocol           = "TCP"
@@ -268,3 +333,5 @@ resource "google_compute_forwarding_rule" "fr-egress-shared-vpc" {
 #     network               = google_compute_network.vpc_single_tenant.id
 #     subnetwork            = google_compute_subnetwork.subnet-i.id
 # }
+# ########################################################################
+# health check ILB EGRESS
--- vpc-routes.tf
+++ vpc-routes.tf
@@ -1,48 +1,50 @@
+############################################################################
 # static routes for VPC-Shared
+############################################################################
 resource "google_compute_route" "east-west-vpc-shared" {
     project     =  module.host_project.name
     name         = "east-west-vpc-shared"
     dest_range   = "192.168.0.0/16"
     network      = google_compute_network.vpc_shared.id
-    next_hop_ilb  = google_compute_forwarding_rule.fr-egress-shared-vpc.id
+    next_hop_ilb  = google_compute_forwarding_rule.ilb-egress-fr-shared-vpc.id
     priority     = 1000
 }
-
 resource "google_compute_route" "north-south-vpc-shared" {
     project     =  module.host_project.name
     name         = "north-south-vpc-shared"
     dest_range   = "0.0.0.0/0"
     network      = google_compute_network.vpc_shared.id
-    next_hop_ilb  = google_compute_forwarding_rule.fr-egress-shared-vpc.id
+    next_hop_ilb  = google_compute_forwarding_rule.ilb-egress-fr-shared-vpc.id
     priority     = 1000
 }
-
-# # static routes for VPC-Multi-Tenant
-# resource "google_compute_route" "east-west-vpc-multi-tenant" {
-#     project     =  module.host_project.name
-#     name         = "east-west-vpc-multi-tenant"
-#     dest_range   = "192.168.0.0/16"
-#     network      = google_compute_network.vpc_multi_tenant.id
-#     next_hop_ilb  = google_compute_forwarding_rule.fr-egress-multi-tenant-vpc.id
-#     priority     = 1000
-# }
-
-# resource "google_compute_route" "north-south-vpc-multi-tenant" {
-#     project     =  module.host_project.name
-#     name         = "north-south-vpc-multi-tenant"
-#     dest_range   = "0.0.0.0/0"
-#     network      = google_compute_network.vpc_multi_tenant.id
-#     next_hop_ilb  = google_compute_forwarding_rule.fr-egress-multi-tenant-vpc.id
-#     priority     = 1000
-# }
-
-# # static routes for VPC-Single-Tenant
+############################################################################
+# static routes for VPC-Multi-Tenant
+############################################################################
+resource "google_compute_route" "east-west-vpc-multi-tenant" {
+    project     =  module.host_project.name
+    name         = "east-west-vpc-multi-tenant"
+    dest_range   = "192.168.0.0/16"
+    network      = google_compute_network.vpc_multi_tenant.id
+    next_hop_ilb  = google_compute_forwarding_rule.ilb-egress-fr-multi-tenant-vpc.id
+    priority     = 1000
+}
+resource "google_compute_route" "north-south-vpc-multi-tenant" {
+    project     =  module.host_project.name
+    name         = "north-south-vpc-multi-tenant"
+    dest_range   = "0.0.0.0/0"
+    network      = google_compute_network.vpc_multi_tenant.id
+    next_hop_ilb  = google_compute_forwarding_rule.ilb-egress-fr-multi-tenant-vpc.id
+    priority     = 1000
+}
+############################################################################
+# static routes for VPC-Single-Tenant
+############################################################################
 # resource "google_compute_route" "east-west-vpc-single-tenant" {
 #     project     =  module.host_project.name
 #     name         = "east-west-vpc-single-tenant"
 #     dest_range   = "192.168.0.0/16"
 #     network      = google_compute_network.vpc_single_tenant.id
-#     next_hop_ilb  = google_compute_forwarding_rule.fr-egress-single-tenant-vpc.id
+#     next_hop_ilb  = google_compute_forwarding_rule.ilb-egress-fr-single-tenant-vpc.id
 #     priority     = 1000
 # }
 
@@ -51,6 +53,6 @@ resource "google_compute_route" "north-south-vpc-shared" {
 #     name         = "north-south-vpc-single-tenant"
 #     dest_range   = "0.0.0.0/0"
 #     network      = google_compute_network.vpc_single_tenant.id
-#     next_hop_ilb  = google_compute_forwarding_rule.fr-egress-single-tenant-vpc.id
+#     next_hop_ilb  = google_compute_forwarding_rule.ilb-egress-fr-single-tenant-vpc.id
 #     priority     = 1000
 # }
